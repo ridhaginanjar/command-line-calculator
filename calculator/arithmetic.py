03a61235f681ca434c105878c55ff6b2d883a7a8
ridhaginanjar <ridhaginanjar7@gmail.com>
2024-10-13 15:18:18 +0700

    Add Arithmetics Operations

---
+++ calculator/arithmetic.py
@@ -0,0 +1,31 @@
+def get_numbers():
+    numbers = input("Masukan angka dengan spasi sebagai pemisahnya (gunakan titik '.' sebagai koma): ")
+    return [float(numbers) for numbers in numbers.split()]
+
+def addition(numbers):
+    result = numbers[0]
+    for number in numbers[1:]:
+        result += number
+    return round(result, 2)
+
+def subtraction(numbers):
+    result = numbers[0]
+    for number in numbers[1:]:
+        result -= number
+    return round(result, 2)
+
+def multiplication(numbers):
+    result = numbers[0]
+    for number in numbers[1:]:
+        result *= number
+    return round(result, 2)
+
+def division(numbers):
+    result = numbers[0]
+    try:
+        for number in numbers[1:]:
+            result /= number
+    except ZeroDivisionError:
+        return "Angka 0 ditemukan! Anda tidak bisa membagi bilangan dengan angka 0."
+
+    return round(result, 2)
---
+++ main.py
@@ -0,0 +1,42 @@
+from calculator import get_numbers, addition, subtraction, multiplication, division
+
+def main():
+    while True:
+        print("==========================")
+        print("Aplikasi Kalkulator")
+        print("==========================")
+        print("Pilih operasi aritmatika!")
+        print("1.Penjumlahan")
+        print("2.Pengurangan")
+        print("3.Perkalian")
+        print("4.Pembagian")
+        print("==========================")
+
+        print("Masukan selain angka di atas jika ingin keluar program!")
+        choice = input("Masukan Pilihan Anda: ")
+        if choice != "1" and choice != "2" and choice != "3" and choice != "4":
+            print("Keluar Program!")
+            break
+
+        try:
+            numbers = get_numbers()
+
+            if choice == "1":
+                print(f"Hasil: {addition(numbers)}")
+                break
+            if choice == "2":
+                print(f"Hasil: {subtraction(numbers)}")
+                break
+            if choice == "3":
+                print(f"Hasil: {multiplication(numbers)}")
+                break
+            if choice == "4":
+                print(f"Hasil: {division(numbers)}")
+                break
+        except ValueError as e:
+            print("Terjadi kesalahan! Pastikan Anda memasukan angka dengan spasi sebagai pemisahnya")
+            break
+
+
+if __name__ == "__main__":
+    main()
