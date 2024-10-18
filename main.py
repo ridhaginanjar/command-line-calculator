4bb6d8b2d28026abfc799178c0b435750fe4647b
ridhaginanjar <ridhaginanjar7@gmail.com>
2024-10-18 21:34:34 +0700

    Add handling when input is equal or less than one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from calculator import get_numbers, addition, subtraction, multiplication, division
+import sys
 
 def main():
     while True:
@@ -20,19 +21,21 @@ def main():
 
         try:
             numbers = get_numbers()
-
-            if choice == "1":
-                print(f"Hasil: {addition(numbers)}")
-                break
-            if choice == "2":
-                print(f"Hasil: {subtraction(numbers)}")
-                break
-            if choice == "3":
-                print(f"Hasil: {multiplication(numbers)}")
-                break
-            if choice == "4":
-                print(f"Hasil: {division(numbers)}")
-                break
+            if len(numbers) <= 1:
+                sys.exit("Anda harus memasukan minimal 2 angka")
+            else:
+                if choice == "1":
+                    print(f"Hasil: {addition(numbers)}")
+                    break
+                if choice == "2":
+                    print(f"Hasil: {subtraction(numbers)}")
+                    break
+                if choice == "3":
+                    print(f"Hasil: {multiplication(numbers)}")
+                    break
+                if choice == "4":
+                    print(f"Hasil: {division(numbers)}")
+                    break
         except ValueError as e:
             print("Terjadi kesalahan! Pastikan Anda memasukan angka dengan spasi sebagai pemisahnya")
             break
